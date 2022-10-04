c36b7f8639d1f9c7c19c96453c46c22b2e9496c4
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-10-04 11:56:31 +0530

    feat: user-validation

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const User = mongoose.Schema({
-    fullname: String,
-    email: String,
-    password: String,
-    phone: Number,
-})
+    fullName: {type: String, required: [true,  'Full Name cannot be empty']},
+    email: {type: String, required: [true, 'Email cannot be empty']},
+    password: {type: String, required: [true, 'Password cannot be empty']},
+    phone: {type: String, required: [true, 'Phone cannot be empty']}
+},
+{
+    timestamps: true
+}
+)
 
-const model = mongoose.model("User", User);
+module.exports = mongoose.model('User', UserSchema);
 
-module.exports = model;
