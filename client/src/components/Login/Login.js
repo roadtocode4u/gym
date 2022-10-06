2ff05c391c609c5c9f258ea00f40533dc2bd1d6e
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-10-06 19:39:33 +0530

    feat: minor changes

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -17,15 +17,15 @@ function Login() {
               <h2 className="text-center mt-5">Login Form</h2>
               <form className="mt-4">
                 <div className="mb-4">
-                  <label for="exampleInputEmail1" className="form-label">Email address</label>
+                  <label for="exampleInputEmail1" className="form-label">Enter email address</label>
                   <input type="email" className="form-control" placeholder='example@gmail.com' id="exampleInputEmail1" />
                 </div>
                 <div className="mb-4">
-                  <label for="exampleInputPassword1" className="form-label">Password</label>
+                  <label for="exampleInputPassword1" className="form-label">Enter password</label>
                   <input type="password" className="form-control" placeholder='Enter password' id="exampleInputPassword1" />
                 </div>
                 <div className='Forget-password'>
-                  <a href="#">Forget password</a>
+                  <a href="#">Forget password?</a>
                 </div>
                 <div>
                   <button type="submit">Log in</button>
