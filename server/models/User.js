10ad6a2ce288952faedb0f2fc8b7661484623017
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-10-06 11:14:14 +0530

    feat: user-validation

--- server/models/User.js
+++ server/models/User.js
@@ -2,14 +2,33 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
     fullName: {type: String, required: [true,  'Full Name cannot be empty']},
-    email: {type: String, required: [true, 'Email cannot be empty'],index:true, unique: true},
-    password: {type: String, required: [true, 'Password cannot be empty']},
-    phone: {type: String, required: [true, 'Phone cannot be empty']}
+    email: {type: String, required: [true, 'Email cannot be empty'],index:true, unique: true, 
+    validate: {
+        validator: function(v) {
+            return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        },
 },
+    password: {type: String, required: [true, 'Password cannot be empty']},
+    phone: {type: String, required: [true, 'Phone cannot be empty'], index:true, unique: true,
+    validate: {
+        validator: function(v) {
+            return /^\d{10}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now},
+});
+
+
 {
     timestamps: true
 }
-)
+
+
 const User = mongoose.model('User', userSchema);
 User.createIndexes();
 export default User;
