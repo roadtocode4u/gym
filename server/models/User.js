93fd6ebadfa9b153dc5582f2281fcbf7751914d8
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-10-08 10:09:16 +0530

    minor changes in user validation

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 const userSchema = mongoose.Schema({
 
-
     fullName: { type: String, required: [true, 'Full Name cannot be empty'], index: true},
     email: { type: String, required: [true, 'Email cannot be empty'], index: true, unique: true,
     validate: {
@@ -20,59 +19,10 @@ const userSchema = mongoose.Schema({
     }},
 });
 
-
-    fullName: {type: String, required: [true,  'Full Name cannot be empty']},
-    email: {type: String, required: [true, 'Email cannot be empty'],index:true, unique: true, 
-
-    fullName: {type: String, required: [true,  'Full Name cannot be empty'], index: true},
-    email: {type: String, required: [true, 'Email cannot be empty'],index:true, unique: true,
-
-    validate: {
-        validator: function(v) {
-            return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
-            },
-
-            message: props => `${props.value} is not a valid email!`
-        },
-},
-
-            message: props => `${props.v} is not a valid email!`
-        }
-    },
-
-    password: {type: String, required: [true, 'Password cannot be empty']},
-    phone: {type: String, required: [true, 'Phone cannot be empty'], index:true, unique: true,
-    validate: {
-        validator: function(v) {
-            return /^\d{10}$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-
-        },
-},
-
-        }
-    },
-
-    createdAt: {type: Date, default: Date.now},
-    updatedAt: {type: Date, default: Date.now},
-});
-
-
-
-
 {
     timestamps: true
 }
 
-
 const User = mongoose.model('User', userSchema);
 User.createIndexes();
 export default User;
-
-
-
-const User = mongoose.model('User', userSchema);
-User.createIndexes();
-export default User;
-
