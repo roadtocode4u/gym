8e1314865b6464f42374b84cc8ab6273ba0bece8
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-10-08 09:42:09 +0530

    changes in login page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -9,13 +9,13 @@ function Login() {
       <div className="log-background">
         <div className="container">
           <div className="row">
+          <h2 className="text-center login-heading mt-2">Login Form</h2>
             <div className="col-md-6">
               <img className="img-fluid mx-auto d-block mt-1 " src={Workout} alt="workout" />
             </div>
 
             <div className="col-md-6">
-              <h2 className="text-center mt-5">Login Form</h2>
-              <form className="mt-4">
+              <form className="mt-5">
                 <div className="mb-4">
                   <label for="exampleInputEmail1" className="form-label">Enter email address</label>
                   <input type="email" className="form-control" placeholder='example@gmail.com' id="exampleInputEmail1" />
@@ -34,7 +34,6 @@ function Login() {
                   <p>Don't have an account? <a  className='acc-signup' href="/signup">Sign up</a></p>
                 </div>
               </form>
-
             </div>
           </div>
         </div>
