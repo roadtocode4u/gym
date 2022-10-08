b27dbae21ffa68d015af6f15a26e3d5a53736853
Yash <naravade.11809605@lpu.in>
2022-10-08 21:17:28 +0530

    Add NavBar

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,26 +9,25 @@ function Home() {
 
 {/* NavBar start */}
 <div class="container-fluid p-0 nav-bar">
-        <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
+        <nav class="navbar navbar-expand-lg py-3 justify-content-between">
           <div className="nav-left">
-            <a href="" class="navbar-brand">
+            <a href="">
             <img src={Logo} alt="" style={{width: "80px"}} />
             </a>
             </div>
-            <div class="collapse navbar-collapse justify-content-between float-right" id="navbarCollapse">
-                <div class="navbar-nav p-4 nav-right float-right">
-                    <a href="index.html" class="nav-item nav-link active">Home</a>
+            <div class="justify-content-between">
+                <div class="navbar-nav p-3 float-right">
+                    <a href="index.html" class="nav-item nav-link active text-light">Home</a>
                     <a href="about.html" class="nav-item nav-link">About Us</a>
                     <a href="feature.html" class="nav-item nav-link">Our Features</a>
                     <a href="class.html" class="nav-item nav-link">Classes</a>
                     <a href="contact.html" class="nav-item nav-link">Contact Us</a>
                 </div>
             </div>
-      
+            
         </nav>
     </div>
     {/* NavBar end */}
-
 </>
 
   )
