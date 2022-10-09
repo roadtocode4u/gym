4c44c7c2e7b4bc589f4a72d33304502da3f72035
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-10-09 10:11:23 +0530

    comment removed

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,12 @@
 import mongoose from 'mongoose';
 const userSchema = mongoose.Schema({
 
-    fullName: { type: String, required: [true, 'Full Name cannot be empty'], index: true},
-    email: { type: String, required: [true, 'Email cannot be empty'], index: true, unique: true,
-    // validate: {
-    //     validator: function(v) {
-    //         return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v); 
-    //     },
-    //     message: props => `${props.value} is not a valid email address!`
-    // }
-},
+    fullName: { type: String, required: [true, 'Full Name cannot be empty'], index: true },
+    email: {
+        type: String, required: [true, 'Email cannot be empty'], index: true, unique: true},
     password: { type: String, required: [true, 'Password cannot be empty'] },
-    phone: { type: String, required: [true, 'Phone number cannot be empty'], index: true, unique: true,
-    // validate: {
-    //     validator: function(v) {
-    //         return /^\d{10}$/.test(v); 
-    //     },
-    //     message: props => `${props.value} is not a valid phone number!`
-    // }
-},
+    phone: {
+        type: String, required: [true, 'Phone number cannot be empty'], index: true, unique: true},
 });
 
 {
