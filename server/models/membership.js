f0d82dec93a480471e825ed2d605783491595a68
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-10-12 09:57:01 +0530

    feat: membership model

--- server/models/membership.js
+++ server/models/membership.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const membershipSchema = mongoose.Schema({
     userReference: {
-        type: Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: [true, "User reference cannot be empty"],
         index: true,
