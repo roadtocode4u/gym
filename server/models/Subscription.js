a3e7115e71cbf6d1a07cb19f41dde18f2bfab986
Yash Naravade <yashdip123@gmail.com>
2022-10-12 21:15:55 +0530

    subs changes 2

--- server/models/Subscription.js
+++ server/models/Subscription.js
@@ -5,8 +5,8 @@ const subscriptionSchema = mongoose.Schema({
     name: { type: String, required: [true, 'Name cannot be empty'],index: true, unique: true},
     email: { type: String, required: [true, 'Email cannot be empty'], index: true, unique: true},
     phone: { type: String, required: [true, 'Phone number cannot be empty'], index: true, unique: true },
-    plan: { type: String, required: [true, 'Plan cannot be empty'], unique: true},
-    duration: { type: String, required: [true, 'Duration cannot be empty'], index: true},
+    plan: { type: String, required: [true, 'Plan cannot be empty']},
+    duration: { type: String, required: [true, 'Duration cannot be empty']},
     amount: { type: String, required: [true, 'Amount cannot be empty']},
     paymentMethod: { type: String, required: [true, 'Payment method cannot be empty']}
 },
