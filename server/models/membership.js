2ca77e5ce7e8d704f5e2ae74f497826224799de2
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-10-14 21:12:22 +0530

    fix: fix changes

--- server/models/membership.js
+++ server/models/membership.js
@@ -9,23 +9,18 @@ const membershipSchema = mongoose.Schema({
     },
     age: {
         type: Number,
-        required: [true, "Age cannot be empty"],
     },
     gender: {
         type: String,
-        required: [true, "Gender cannot be empty"],
     },
     height: {
         type: Number,
-        required: [true, "Height cannot be empty"],
     },
     weight: {
         type: Number,
-        required: [true, "Weight cannot be empty"],
     },
     BMI: {
         type: Number,
-        required: [true, "BMI cannot be empty"],
     },
     timeSlot: {
         type: String,
@@ -40,12 +35,14 @@ const membershipSchema = mongoose.Schema({
         type: Boolean,
         default: true
     },
-    subscription: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Subscription",
-        required: [true, "Subscription reference cannot be empty"],
-        index: true,
-    },
+    subscription: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Subscription",
+            required: [true, "Subscription reference cannot be empty"],
+            index: true,
+        }
+    ]
 });
 
 const Membership = mongoose.model("Membership", membershipSchema);
