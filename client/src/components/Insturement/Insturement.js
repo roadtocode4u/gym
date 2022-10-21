3725a8c1d1ddd69dc4324e6bb62b131c375e39e6
vaishnavihole <vaishnavihole1@gmail.com>
2022-10-21 07:00:24 +0530

    added insturement menu

---
+++ client/src/components/Insturement/Insturement.js
@@ -0,0 +1,160 @@
+import React from "react";
+import "./Insturement.css";
+import InsturementImg1 from "./img/Barbell.png";
+
+
+
+export default function Insturement() {
+  return (
+    <>
+      <h1 className="text-center  p-2 insturement-title mt-2"> Let's Know About Gym Insturement⛹️‍♀️</h1>
+      <div className="container">
+        <div>
+          <h2>Machine Name : Barbell</h2>
+
+          <h2>Description</h2>
+          <p className="paragraph-text mt-4">
+          The Home Gym All-In-One machine also called a multi-gym is a multi-station workout machine that combines several exercises into one piece of equipment. It’s especially good for circuit training because you can quickly move from one exercise to another.
+          </p>
+          <div className="text-center mt-4">
+          <img className="img-fluid  insturement-img" src={InsturementImg1} alt="" />
+          </div>
+        </div>
+        <div>
+          <h1 className="text-center h1-heading mt-5">
+            <b>Top three foods for muscle building</b>
+          </h1>
+          <li className=" nutrian-menu mt-3">
+            <b>Eggs</b>
+          </li>
+          {/* <div className="text-center mt-4">
+           <img className=" img-fluid nutrains-img" src={NutrainsImg1} alt="" /> 
+          </div> */}
+          <p className="paragraph-text mt-4">
+          No wonder why whenever there is a discussion about gym or muscles building, eggs are the first thing that cracks in the mind as they are a synonym to the word protein and your body needs protein to build muscle mass and initiate muscle protein synthesis for tissues and muscle development.</p>
+         <p className="paragraph-text"> one whole egg provides your body with the high-quality 6gm of protein along with each and every vitamin and mineral that your body requires to function optimally</p> 
+         <p className="paragraph-text"> Apart from this your body also gets the required omega 3 fatty acids for good hormones production and skin and hair health, and remember that fats are important for your body, they are needed for good hormones production that indirectly impacts your mood and muscles growth progress.</p>
+          <p className="paragraph-text">You can consume eggs based on your daily protein requirement, as one whole egg gives about 6gm of protein, whereas one egg white gives 3.5gm of protein, which you can easily integrate based on your diet and macros.</p>
+           <li className="nutrian-menu mt-3">
+            <b> Chicken</b>
+          </li>
+          {/* <div className="text-center mt-4">
+           <img className="img-fluid nutrains-img" src={NutrainsImg2} alt="" /> 
+           </div> */}
+          <p className="paragraph-text mt-4">We guess most people knew that this food would be on the list, chicken is one of the most popular foods items used in bodybuilding or healthy lifestyle, basically due to the reasons like it offers a high amount of protein per serving, which is you get about 27gm of complete protein per 100gm of chicken breast. </p>
+          <p className="paragraph-text"> The combination of chicken and rice is hands down the best combo that any bodybuilder can add into their diet, chicken being a lean source of protein, which is it only offers the best protein with negligible fats or carbs so that one can prepare for their fitness transformation effectively.</p>
+          <p className="paragraph-text"> Also chicken is easily available in any part of the world, easy to cook as well and taste just amazing if you know how to make a healthy version using great spices and fewer oils/butter.  </p>
+          <p className="paragraph-text"> Also since it takes some effort to chew chicken, it also helps in reducing the overall calories consumption of a person as it reduces one's hunger and eventually acts as great food to be added to the weight loss journey. </p>
+          <li className="nutrian-menu mt-3">
+            <b>Soya chunks</b>
+          </li>
+          {/* <div className="text-center mt-4">
+          <img className="img-fluid nutrains-img" src={NutrainsImg3} alt="" /> 
+          </div> */}
+          <p className="paragraph-text mt-4">If there is anything that some fitness enthusiasts are scared of other than fake whey protein is soya chunks and its cause to hormonal imbalance, for the 100th time we are putting on this record that there is no specific link of consuming soya chunks in a limited quantity with the rise of estrogen in males body.</p>
+          <p className="paragraph-text">There is absolutely no evidence that supports this rumor and hence you should also not fear soya products!</p>
+          <p className="paragraph-text">as it is proven that one has to eat an absolutely crazy amount of soya chunks to get his hormones misplaces, which s beyond the limit of any regular person, hence one can safely and without any worries can add soya to their diet as soya chunks are one of the highest sources of protein out there.</p>
+          <p className="paragraph-text">You get about 52gm of complete protein in 100gm of soya chunks, which again blends perfectly with the Indian diet, is easy to cook, and affordable as well It's a win-win situation to add soya chunks in who is looking to pack on some muscle mass. Also, don't forget that you get all 9 essential amino acids for effective recovery.</p>
+          <div>
+            <h2>Best Nutrians to Help Weight Increase...</h2>
+            {/* <img src={ExciseImg5}
+            className="img-fluid" alt="Responsive Image"
+            width="800" /> */}
+            {/* <img src={ExciseImg5} className="" alt="Responsive image"/> */}
+            <p className="paragraph-text mt-4">
+            For some people, gaining weight or adding muscle can be just as difficult as losing weight is for others.
+            </p>
+            <p className="paragraph-text mt-4">
+            However, simply adding certain foods to your diet can make your weight gain efforts both healthy and more effective.
+            </p>
+          </div>
+
+          <div>
+            <h1 className="text-center h1-heading mt-5">
+              <b>Nutrians to gain weight for females and males</b>
+            </h1>
+            <li className="nutrian-menu mt-3">
+              <b>Milk</b>
+            </li>
+           {/* <div className="text-center mt-4">
+           <img className="img-fluid nutrains-img" src={NutrainsImg4} alt="" /> 
+           </div> */}
+            <p className="paragraph-text mt-4">
+            Milk has been used as a weight gainer or muscle builder for decades
+            </p>
+            <ol>
+              <li className="paragraph-text">It provides a good balance of proteins, carbs, and fats and is a good source of calcium, as well as other vitamins and minerals.</li>
+              <li className="paragraph-text">
+              For those trying to add more muscle, milk is an excellent protein source that provides both casein and whey proteins. Research has even shown it can help you add muscle when combined with weightlifting.
+              </li>
+              <li className="paragraph-text">
+              Additionally, studies have found that milk, or whey and casein combined, can lead to greater mass gain than other protein sources.
+              </li>
+              <li className="paragraph-text">
+              Try drinking one or two glasses of whole milk (149 calories per cup) as a snack, with a meal, or before and after a workout if you’re training.
+              </li>
+              <li className="paragraph-text">
+              Milk smoothies are also a delicious way to add milk to your diet. For an easy morning protein boost, try blending up 1 cup of frozen berries, 1 cup of whole milk, 2 teaspoons of honey, and 1 teaspoon of vanilla (about 275 calories).
+              </li>
+            </ol>
+
+            <li className="nutrian-menu mt-3">
+              <b>Rice</b>
+            </li>
+            {/* <div className="text-center mt-4">
+           <img className="img-fluid nutrains-img" src={NutrainsImg5} alt="" /> 
+           </div> */}
+            <p className="paragraph-text mt-4">
+            Rice is a convenient, low cost carb source to help you gain weight. Just 1 cup (158 grams) of cooked white rice provides 204 calories, 44 grams of carbs, and very little fat.
+            </p>
+            <ol>
+              <li className="paragraph-text ">
+              Rice is also fairly calorie-dense, meaning you can easily obtain a high amount of carbs and calories from a single serving. This helps you eat more food, especially if you have a poor appetite or get full quickly.
+              </li>
+              <li className="paragraph-text">
+              When you’re on the go or in a rush, 2-minute packs of microwavable rice can be easily added to other protein sources and premade meals.
+              </li>
+              <li className="paragraph-text">
+              Another popular method is to prepare a large pot of rice, refrigerate it or freeze individual portions, and then combine it with different proteins and healthy fats for varied meals throughout the week
+              </li>
+              <li className="paragraph-text">
+              There are many ways to turn relatively bland rice into a taste extravaganza. The easiest way to add taste, calories, and a protein boost is to simply stir in some of these ingredients after you have cooked your rice:
+              </li>
+              <ol c>
+                <li className="paragraph-text">butter and Parmesan cheese</li>
+                <li className="paragraph-text">broccoli and cheese</li>
+                <li className="paragraph-text">scrambled eggs</li>
+                <li className="paragraph-text">toasted sesame seeds, peanuts, or cashews</li>
+              </ol>
+              
+            </ol>
+
+            <li className="nutrian-menu mt-3">
+              <b>Nuts and nut butters</b>
+            </li>
+            {/* <div className="text-center mt-4">
+           <img className="img-fluid nutrains-img" src={NutrainsImg6} alt="" /> 
+           </div> */}
+            <p className="paragraph-text mt-4">
+            Nuts and nut butters are perfect choices if you’re looking to gain weight.
+            </p>
+            <ol>
+              <li className="paragraph-text">
+              Since nuts are very calorie-dense, just two handfuls per day with a meal or as a snack can quickly add hundreds of calories.
+              </li>
+              <li className="paragraph-text">
+              You can add nut butters to a variety of snacks or dishes, such as smoothies, yogurts, and crackers, to turn them into a high calorie snack in no time.
+              </li>
+              <li className="paragraph-text">
+              For a quick pick-me-up, try this peanut butter banana smoothie, with only three ingredients (270 calories, using whole milk). If you have a peanut allergy, substitute another nut butter.
+              </li>
+              <li className="paragraph-text">
+              However, make sure you pick 100 percent nut butters with no added sugar or extra oils. Or better yet, make your own from this homemade almond butter recipe. It’s easy to make and easy on your wallet, too.
+              </li>
+              </ol>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
